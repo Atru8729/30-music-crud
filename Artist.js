73f73d3dc098e5dffbe37de57fa62b5ecd686d81
Atru8729 <a.truskauskas1@gmail.com>
2021-07-23 20:43:31 +0300

    final

--- Artist.js
+++ Artist.js
@@ -2,36 +2,40 @@ class Artist {
     constructor(atlikejas, currency) {
         this.atlikejas = atlikejas;
         this.currency = currency;
-        this.dainosList = [];
+        this.playList = [];
+        this.priceOneSong = 0;
+        this.wealth = 0;
 
     }
     intro() {
         console.log(`Hi, my name is ${this.atlikejas} and I am a musician!`);
     }
-    songPrice(priceInCents) {
-        const price = (priceInCents / 100).toFixed(2);
-        console.log(`Raimonda's price per song is ${price} ${this.currency}`);
+    songPrice(price) {
+        this.priceOneSong = price;
+        const priceOneSong = (price / 100).toFixed(2);
+        console.log(`${this.atlikejas}'s price per song is ${priceOneSong} ${this.currency}`);
     }
     addSong(dainosPavadinimas) {
-        this.dainosList.push({ dainosPavadinimas, grojimuKiekis: 0 });
+        this.playList.push({ dainosPavadinimas, count: 0 });
         console.log(dainosPavadinimas);
     }
     playlist() {
         console.log(`${this.atlikejas}'s playlist:`);
         console.log(`====================`);
         let count = 0;
-        for (let i = 0; i < this.dainosList.length; i++) {
-            const daina = this.dainosList[i];
+        for (let i = 0; i < this.playList.length; i++) {
+            const daina = this.playList[i];
             count++;
-            console.log(`${count}. ${daina.dainosPavadinimas} (${daina.grojimuKiekis})`);
+            console.log(`${count}. ${daina.dainosPavadinimas} (${daina.count})`);
         }
     }
     playSong(songNumber) {
-        ++this.dainosList[songNumber].count;
-        console.log(`Playing song: ${this.dainosList[songNumber].dainosPavadinimas}.`);
+        ++this.playList[songNumber].count;
+        this.wealth += this.priceOneSong;
+        console.log(`Playing song: ${this.playList[songNumber].dainosPavadinimas}.`);
     }
     fortune() {
-        console.log(`Total lifetime wealth of ${this.atlikejas} is 1.05 ${this.currency} right now!`);
+        console.log(`Total lifetime wealth of ${this.atlikejas} is ${(this.wealth / 100).toFixed(2)} ${this.currency} right now!`);
     }
 }
 
